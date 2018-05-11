88defeb471b49a7714641c7ddd62b928adfbbd88
Isaac Khan <36248421+IsaacKhan@users.noreply.github.com>
2018-05-10 22:05:43 -0500

    table formatting adjustmnet

--- Query2.php
+++ Query2.php
@@ -39,10 +39,7 @@ if($result->num_rows > 0){
     }
 else 
     {
-        echo "<br>";
-        echo "Top 20 for ". $row["State"];
-        echo "<br>";
-
+        echo "<tr><td>" . "Top 20 for ". $row["State"] . "</td></tr>";
         $rank = 1;
     }
 

3dce53495a9ab63bb8d9728a440898569789047c
Isaac Khan <36248421+IsaacKhan@users.noreply.github.com>
2018-05-10 21:55:52 -0500

    test query update

--- Query1.php
+++ Query1.php
@@ -1,18 +1,23 @@
 <?php
 $mysqli = new mysqli("coffee-gave-me-gas.cgzqmhf3sjbn.us-east-2.rds.amazonaws.com", "root", "csc4112018", "projectcoffee");
-$result = $mysqli->query("SELECT Rank, productName AS Product, units AS 'Units Sold', storeName AS Store
-FROM (SELECT product_ID AS upc, amountSold AS units_sold, store_ID, storeName,
-        -- Leveraged mysql session variables to track ranking 
-        -- If the store_ID is = to the previous one than increase the rank, otherwise start back at 1
-          @cur_rank := IF(@store = store_ID, @cur_rank + 1, 1) AS Rank, 
-          @store := store_ID,
-          @units := amountSold AS units
-            -- In order to get the correct ranking order a Subquery of each stores products sales in descending order is needed
-            FROM  (SELECT product_ID, amountSold, store_ID, storeName
-                        FROM sales INNER JOIN store on store.ID = sales.store_ID
-                        ORDER BY store_ID, amountSold desc) AS top_items) AS rankings INNER JOIN product ON upc = product.ID
-        -- Cap the ranking at the top 20 items
-        WHERE rank <= 20 ");
+$result = $mysqli->query("SELECT @cur_rank, productName as Product, units as 'Units Sold', storeName as Store, store_ID
+from (select product_ID as upc, amountSold as units_sold, store_ID, storeName,
+            -- Leveraged mysql session variables to track ranking 
+            -- If the store_ID is = to the previous one than increase the rank, otherwise start back at 1
+              @cur_rank := IF(@store = store_ID, @cur_rank + 1, 1) as Rank, 
+              @store := store_ID,
+              @units := amountSold as units
+                -- In order to get the correct ranking order a Subquery of each stores products sales in descending order is needed
+                from  (    select product_ID, amountSold, store_ID, storeName
+                            from sales inner join store on store.ID = sales.store_ID
+                            order by store_ID, amountSold desc) as top_items
+) as rankings inner join product on upc = product.ID
+-- Cap the ranking at the top 20 items
+where @cur_rank <= 20;");
+
+$prev_store = "";
+$prev_prod = "";
+$rank = 1;
 ?>
 
 <!doctype html>
@@ -28,17 +33,27 @@ FROM (SELECT product_ID AS upc, amountSold AS units_sold, store_ID, storeName,
 </tr>
 <?php
 //Fetch Data form database
-if($result->num_rows > 0){
- while($row = $result->fetch_assoc()){
- ?>
- <tr>
- <td><?php echo $row['Rank']; ?></td>
- <td><?php echo $row['Product']; ?></td>
- <td><?php echo $row['Units Sold']; ?></td>
- <td><?php echo $row['Store']; ?></td>
- </tr>
- <?php
- }
+if($result->num_rows > 0)
+{
+  while($row = $result->fetch_assoc())
+  {
+    if ($prev_store == $row["store_ID"])
+      $rank++;
+    else 
+    {
+      echo "<br>";
+      echo "Top 20 for ". $row["Store"]. " ". $row["store_ID"];
+      echo "<br>";
+
+      $rank = 1;
+    }
+
+    #echo $rank . ".) " . $row["Product"] . " " . $row["Units Sold"]  . " " . $row["Store"] ." <br>"; 
+    echo "<tr><td>" . $row['Rank'] . "</td><td>" . $row['Product'] . "</td><td>" . $row['Units Sold'] . "</td><td>" . $row['Store'] . "</td></tr>";
+    
+    $prev_store = $row["store_ID"];
+ 
+  }
 }
 else
 {
